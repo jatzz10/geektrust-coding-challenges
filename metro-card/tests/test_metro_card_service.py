3a0a60d1bb6b7250822cd54196319296f4eab40c
Jatin Sharma <41716010+jatzz10@users.noreply.github.com>
2024-09-07 01:18:37 +0530

    Add tests (#3)
    * Add tests
    
    * Fix path import issue

---
+++ metro-card/tests/test_metro_card_service.py
@@ -0,0 +1,66 @@
+import unittest
+from unittest.mock import patch
+from src.metro_card_service import MetroCardService
+from src.passenger import Passenger
+from src.constants import DISCOUNT_DIVISOR
+
+
+class TestMetroCardServices(unittest.TestCase):
+    def setUp(self):
+        self.metro_card_service = MetroCardService()
+
+    def test_add_card_balance(self):
+        self.metro_card_service.add_card_balance("MC1", 200)
+        self.assertEqual(self.metro_card_service.get_metro_card_dict()["MC1"].get_balance(), 200)
+
+    def test_calculate_travel_charge(self):
+        self.assertEqual(self.metro_card_service.calculate_travel_charge('ADULT'),
+                         Passenger('ADULT').get_travel_charge_based_on_passenger_type())
+
+    def test_calculate_discount_for_one_way_journey(self):
+        self.metro_card_service.add_card_balance("MC1", 200)
+        self.assertEqual(self.metro_card_service.calculate_discount(100, "MC1"), 0)
+
+    def test_calculate_discount_for_return_journey(self):
+        self.metro_card_service.add_card_balance("MC1", 200)
+        self.metro_card_service.get_metro_card_dict()["MC1"].set_card_swiped_for_one_way(True)
+        self.assertEqual(self.metro_card_service.calculate_discount(100, "MC1"), 100 // DISCOUNT_DIVISOR)
+
+    def test_add_recharge_fee_for_insufficient_balance(self):
+        self.metro_card_service.add_card_balance("MC1", 200)
+        self.assertEqual(self.metro_card_service.add_recharge_fee_if_insufficient_balance("MC1", 250), 251)
+        self.assertEqual(self.metro_card_service.get_metro_card_dict()["MC1"].get_balance(), 250)
+
+    def test_add_recharge_fee_for_sufficient_balance(self):
+        self.metro_card_service.add_card_balance("MC1", 200)
+        self.assertEqual(self.metro_card_service.add_recharge_fee_if_insufficient_balance("MC1", 150), 150)
+        self.assertEqual(self.metro_card_service.get_metro_card_dict()["MC1"].get_balance(), 200)
+
+    def test_update_station_stats(self):
+        self.metro_card_service.update_station_stats("CENTRAL", "ADULT", 100, 0)
+        self.assertEqual(self.metro_card_service.get_station_stats_dict()["CENTRAL"].get_total_amount_collected(), 100)
+        self.assertEqual(self.metro_card_service.get_station_stats_dict()["CENTRAL"].get_total_discount_given(), 0)
+        self.assertEqual(self.metro_card_service.get_station_stats_dict()['CENTRAL'].get_passenger_type_count(), {"ADULT": 1})
+
+    def test_update_metro_card(self):
+        self.metro_card_service.add_card_balance("MC1", 100)
+        self.metro_card_service.update_metro_card("MC1", 50)
+        self.assertEqual(self.metro_card_service.get_metro_card_dict()["MC1"].get_balance(), 50)
+        self.assertTrue(self.metro_card_service.get_metro_card_dict()["MC1"].get_card_swiped_for_one_way())
+
+    def test_perform_check_in(self):
+        self.metro_card_service.add_card_balance("MC1", 200)
+        self.metro_card_service.perform_check_in("MC1", "ADULT", "CENTRAL")
+        self.assertEqual(len(self.metro_card_service.get_metro_card_dict()), 1)
+        self.assertEqual(self.metro_card_service.get_metro_card_dict()["MC1"].get_balance(), 0)
+        self.assertEqual(self.metro_card_service.get_metro_card_dict()["MC1"].get_card_swiped_for_one_way(), True)
+        self.assertEqual(self.metro_card_service.get_station_stats_dict()["CENTRAL"].get_total_amount_collected(), 200)
+        self.assertEqual(self.metro_card_service.get_station_stats_dict()["CENTRAL"].get_total_discount_given(), 0)
+        self.assertEqual(self.metro_card_service.get_station_stats_dict()["CENTRAL"].get_passenger_type_count(), {"ADULT": 1})
+
+    def test_print_summary(self):
+        self.metro_card_service.add_card_balance("MC1", 100)
+        self.metro_card_service.perform_check_in("MC1", "ADULT", "CENTRAL")
+        with patch('builtins.print') as mock_print:
+            self.metro_card_service.print_summary()
+            mock_print.assert_called()
---
+++ metro-card/tests/test_passenger.py
@@ -0,0 +1,19 @@
+import unittest
+from src.passenger import Passenger, PassengerTravelCharge
+
+
+class TestPassenger(unittest.TestCase):
+    def init(self):
+        passenger = Passenger('ADULT')
+        self.assertEqual(passenger.get_passenger_type(), 'ADULT')
+
+    def test_get_travel_charge_based_on_passenger_type(self):
+        adult_passenger = Passenger('ADULT')
+        self.assertEqual(adult_passenger.get_travel_charge_based_on_passenger_type(), PassengerTravelCharge.ADULT.value)
+
+        senior_citizen_passenger = Passenger('SENIOR_CITIZEN')
+        self.assertEqual(senior_citizen_passenger.get_travel_charge_based_on_passenger_type(),
+                         PassengerTravelCharge.SENIOR_CITIZEN.value)
+
+        kid_passenger = Passenger('KID')
+        self.assertEqual(kid_passenger.get_travel_charge_based_on_passenger_type(), PassengerTravelCharge.KID.value)
